d011cf653ef6ca078740f5aafbd1b8c27bee0fc6
Prateeek73 <singh.prateek2599@gmail.com>
2020-10-26 19:17:31 +0530

    Minor changes to leaderboard

--- app/src/main/java/com/orion/orion/Adapters/AdapterItemLeaderboard.java
+++ app/src/main/java/com/orion/orion/Adapters/AdapterItemLeaderboard.java
@@ -2,12 +2,10 @@ package com.orion.orion.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,19 +13,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.api.LogDescriptor;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 import com.orion.orion.R;
-import com.orion.orion.explore.Explore;
 import com.orion.orion.models.ItemLeaderboard;
-import com.orion.orion.models.Leaderboard;
-import com.orion.orion.models.ParticipantList;
-import com.orion.orion.models.users;
 import com.orion.orion.profile.profile;
 import com.orion.orion.util.UniversalImageLoader;
 
@@ -35,45 +22,45 @@ import java.util.ArrayList;
 
 import static android.content.ContentValues.TAG;
 
-public class AdapterItemLeaderboard extends RecyclerView.Adapter<AdapterItemLeaderboard.ViewHolder>{
+public class AdapterItemLeaderboard extends RecyclerView.Adapter<AdapterItemLeaderboard.ViewHolder> {
 
     private ArrayList<ItemLeaderboard> mList;
     Context mContext;
+
     public AdapterItemLeaderboard(ArrayList<ItemLeaderboard> mList, Context mContext) {
         this.mList = mList;
-        this.mContext=mContext;
-
+        this.mContext = mContext;
     }
 
     @NonNull
     @Override
     public AdapterItemLeaderboard.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_leaderboard, parent, false);
-        return new ViewHolder(view);
+        if (viewType == 1) return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_top_leaderboard, parent, false));
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_leaderboard, parent, false));
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0 || position == 1 || position == 2) return 1;
+        else return 2;
     }
 
     @Override
     public void onBindViewHolder(@NonNull AdapterItemLeaderboard.ViewHolder holder, int position) {
         ItemLeaderboard currentItem = mList.get(position);
 
-        holder.Position.setText(Integer.toString(position+1));
+        String rank = Integer.toString(position + 1);
+        holder.Position.setText(rank);
         holder.PositionName.setText(currentItem.getPostionName());
         holder.PositionParameter.setText(currentItem.getPostionParameter());
-        Log.i("sakdada", "onBindViewHolder: "+currentItem.getPostionProfile());
-        UniversalImageLoader.setImage(currentItem.getPostionProfile(),holder.PositionProfile,null,"");
-        if(position==0 || position==1 || position==2)
-            holder.PositionCard.setBackgroundResource(R.drawable.vertical_gradient);
-
-
+        UniversalImageLoader.setImage(currentItem.getPostionProfile(), holder.PositionProfile, null, "");
         holder.itemView.setOnClickListener(v -> {
-            Log.d(TAG, "onBindViewHolder: "+currentItem.getUserID());
-
-                    Intent i = new Intent(mContext, profile.class);
-                    i.putExtra(mContext.getString(R.string.calling_activity), mContext.getString(R.string.home));
-                    i.putExtra(mContext.getString(R.string.intent_user), currentItem.getUserID());
-                    mContext.startActivity(i);
-
-
+            Log.d(TAG, "onBindViewHolder: UserID" + currentItem.getUserID());
+            Intent i = new Intent(mContext, profile.class);
+            i.putExtra(mContext.getString(R.string.calling_activity), mContext.getString(R.string.home));
+            i.putExtra(mContext.getString(R.string.intent_user), currentItem.getUserID());
+            mContext.startActivity(i);
         });
     }
 
@@ -81,6 +68,7 @@ public class AdapterItemLeaderboard extends RecyclerView.Adapter<AdapterItemLead
         ItemLeaderboard board = mList.get(position);
         return board.getUserID().hashCode();
     }
+
     @Override
     public int getItemCount() {
         return mList.size();
@@ -97,16 +85,11 @@ public class AdapterItemLeaderboard extends RecyclerView.Adapter<AdapterItemLead
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
-
-            Position=itemView.findViewById(R.id.position);
-            PositionName=itemView.findViewById(R.id.positionName);
-            PositionParameter=itemView.findViewById(R.id.positionPararmeter);
-            PositionProfile=itemView.findViewById(R.id.positionProfile);
-            PositionCard=itemView.findViewById(R.id.leaderboardCard);
-
-
-
+            Position = itemView.findViewById(R.id.position);
+            PositionName = itemView.findViewById(R.id.positionName);
+            PositionParameter = itemView.findViewById(R.id.positionPararmeter);
+            PositionProfile = itemView.findViewById(R.id.positionProfile);
+            PositionCard = itemView.findViewById(R.id.leaderboardCard);
         }
     }
 }
