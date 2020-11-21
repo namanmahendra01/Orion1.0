1c068d7deb14ecbf38238364039ced98b3d450da
Prateek SIngh <singh.prateek2599@gmail.com>
2020-11-21 23:29:04 +0530

    Bug fixes Minor chages

--- app/src/main/java/com/orion/orion/contest/joined/fragment_marks_and_votes.java
+++ app/src/main/java/com/orion/orion/contest/joined/fragment_marks_and_votes.java
@@ -1,19 +1,26 @@
 package com.orion.orion.contest.joined;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.GridView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.orion.orion.Adapters.AdapterVoterList;
+import com.orion.orion.R;
+import com.orion.orion.models.juryMarks;
+import com.orion.orion.models.users;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,45 +28,18 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-import com.orion.orion.Adapters.AdapterContestCreated;
-import com.orion.orion.Adapters.AdapterRankList;
-import com.orion.orion.Adapters.AdapterRankListFull;
-import com.orion.orion.Adapters.AdapterVoterList;
-import com.orion.orion.R;
-import com.orion.orion.contest.ranking;
-import com.orion.orion.models.CreateForm;
-import com.orion.orion.models.ParticipantList;
-import com.orion.orion.models.juryMarks;
-import com.orion.orion.models.users;
-import com.orion.orion.profile.profile;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 import static com.android.volley.VolleyLog.TAG;
 
 public class fragment_marks_and_votes extends Fragment {
 
-    private TableLayout juryTable,juryTable2;
-    private int participants=0;
-    String joiningKey ="";
+    private TableLayout juryTable, juryTable2;
+    private int participants = 0;
+    String joiningKey = "";
     RecyclerView votesRv;
     private ArrayList<String> votingLists;
     private ArrayList<String> paginatedVotingList;
     int mResults;
     users user = new users();
-
-
 
 
     private AdapterVoterList voterList;
@@ -75,21 +55,20 @@ public class fragment_marks_and_votes extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_marks_and_votes, container, false);
 
-        juryTable=view.findViewById(R.id.jurytable);
+        juryTable = view.findViewById(R.id.jurytable);
 
         juryTable.setStretchAllColumns(true);
-        Bundle b=getActivity().getIntent().getExtras();
-        Conteskey=b.getString("contestId");
-        joiningKey=b.getString("joiningKey");
+        Bundle b = getActivity().getIntent().getExtras();
+        Conteskey = b.getString("contestId");
+        joiningKey = b.getString("joiningKey");
 
-        juryMarksTable(Conteskey,joiningKey);
+        juryMarksTable(Conteskey, joiningKey);
 
 
-        votesRv=view.findViewById(R.id.votelistRv);
+        votesRv = view.findViewById(R.id.votelistRv);
         votesRv.setHasFixedSize(true);
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         votesRv.setLayoutManager(linearLayoutManager);
-
 
 
         votesRv.addOnScrollListener(new RecyclerView.OnScrollListener() {
@@ -105,10 +84,9 @@ public class fragment_marks_and_votes extends Fragment {
             }
         });
 
-        votingLists=new ArrayList<>();
+        votingLists = new ArrayList<>();
 
-        getRank(Conteskey,joiningKey);
-
+        getRank(Conteskey, joiningKey);
 
 
         return view;
@@ -124,20 +102,24 @@ public class fragment_marks_and_votes extends Fragment {
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                     juryMarks   juryMarks=dataSnapshot.getValue(juryMarks.class);
-                        Log.d(TAG, "onDataChange: kkk"+juryMarks.toString());
-                        TableRow row1= (TableRow)juryTable.getChildAt(1);
-                        TableRow row2= (TableRow)juryTable.getChildAt(2);
-                        TableRow row3= (TableRow)juryTable.getChildAt(3);
-                        TextView t1=(TextView ) row1.getChildAt(1);
-                        TextView t2=(TextView ) row1.getChildAt(2);
-                        TextView t3=(TextView ) row2.getChildAt(1);
-                        TextView t4=(TextView ) row2.getChildAt(2);
-                        TextView t5=(TextView ) row3.getChildAt(1);
-                        TextView t6=(TextView ) row3.getChildAt(2);
+                        juryMarks juryMarks = dataSnapshot.getValue(juryMarks.class);
+                        Log.d(TAG, "onDataChange: kkk" + juryMarks.toString());
+                        TableRow row1 = (TableRow) juryTable.getChildAt(1);
+                        TableRow row2 = (TableRow) juryTable.getChildAt(2);
+                        TableRow row3 = (TableRow) juryTable.getChildAt(3);
+                        TextView t1 = (TextView) row1.getChildAt(1);
+                        TextView t2 = (TextView) row1.getChildAt(2);
+                        TextView t3 = (TextView) row2.getChildAt(1);
+                        TextView t4 = (TextView) row2.getChildAt(2);
+                        TextView t5 = (TextView) row3.getChildAt(1);
+                        TextView t6 = (TextView) row3.getChildAt(2);
                         t1.setText(juryMarks.getJ1());
+                        if(juryMarks.getJ1().equals("")) t1.setText("-");
                         t3.setText(juryMarks.getJ2());
+                        if(juryMarks.getJ2().equals("")) t3.setText("-");
                         t5.setText(juryMarks.getJ3());
+                        if(juryMarks.getJ3().equals("")) t5.setText("-");
+
 
                         t2.setText(juryMarks.getC1());
                         t4.setText(juryMarks.getC2());
@@ -154,11 +136,9 @@ public class fragment_marks_and_votes extends Fragment {
     }
 
 
-
-
     private void getRank(String contestkey, String joiningKey) {
 
-        DatabaseReference reference =FirebaseDatabase.getInstance().getReference();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
         reference.child(getString(R.string.dbname_participantList))
                 .child(contestkey)
                 .child(joiningKey)
@@ -167,21 +147,20 @@ public class fragment_marks_and_votes extends Fragment {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         votingLists.clear();
-                        int x=0;
-                        for (DataSnapshot snapshot:dataSnapshot.getChildren()){
+                        int x = 0;
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                             x++;
-                            String votingList= snapshot.getKey();
+                            String votingList = snapshot.getKey();
 
 
                             votingLists.add(votingList);
-                            if (x==dataSnapshot.getChildrenCount()){
+                            if (x == dataSnapshot.getChildrenCount()) {
                                 Collections.reverse(votingLists);
 
                                 displayVoterList();
                             }
 
                         }
-
 
 
                     }
@@ -192,6 +171,7 @@ public class fragment_marks_and_votes extends Fragment {
                     }
                 });
     }
+
     private void displayVoterList() {
         paginatedVotingList = new ArrayList<>();
         if (votingLists != null) {
@@ -207,7 +187,7 @@ public class fragment_marks_and_votes extends Fragment {
                 for (int i = 0; i < iteration; i++) {
                     paginatedVotingList.add(votingLists.get(i));
                 }
-                voterList = new AdapterVoterList(getContext(),votingLists);
+                voterList = new AdapterVoterList(getContext(), votingLists);
                 voterList.setHasStableIds(true);
                 votesRv.setAdapter(voterList);
 
@@ -238,11 +218,11 @@ public class fragment_marks_and_votes extends Fragment {
                     paginatedVotingList.add(votingLists.get(i));
 
                 }
-                int positionStart =mResults;
+                int positionStart = mResults;
                 votesRv.post(new Runnable() {
                     @Override
                     public void run() {
-                        voterList.notifyItemRangeInserted(positionStart,iterations);
+                        voterList.notifyItemRangeInserted(positionStart, iterations);
                     }
                 });
 
