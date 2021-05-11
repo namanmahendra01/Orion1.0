7c1fa32a27c50dad4427f8d96c6dace0157d1990
Prateek SIngh <50133080+Prateeek73@users.noreply.github.com>
2021-05-11 17:50:01 +0530

    Added activities

---
+++ app/src/main/java/com/orion/orion/MessagesActivity.java
@@ -0,0 +1,295 @@
+package com.orion.orion;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.orion.orion.Adapters.AdaterChatList;
+import com.orion.orion.home.MainActivity;
+import com.orion.orion.home.Message_Request;
+import com.orion.orion.login.login;
+import com.orion.orion.models.Chat;
+import com.orion.orion.util.BottomNaavigationViewHelper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MessagesActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "ChatActivity";
+    private static final int ACTIVITY_NUM = 1;
+    private Context mContext;
+
+
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private FirebaseUser mUser;
+    private int mResults;
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseUser currentUser;
+
+    private  int x=0;
+    private TextView request;
+    private RecyclerView recyclerView;
+    private List<Chat> chatList;
+    private List<String> userlist;
+    private List<String> userlist2;
+    private List<String> paginateduserList;
+    private AdaterChatList adaterChatList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages);
+        mContext = MessagesActivity.this;
+        recyclerView = findViewById(R.id.recyclerview);
+        request = findViewById(R.id.msgRequest);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        setupBottomNavigationView();
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (!recyclerView.canScrollVertically(1) && newState == RecyclerView.SCROLL_STATE_IDLE)
+                    displayMoreChatList();
+            }
+        });
+        myRef = FirebaseDatabase.getInstance().getReference(getString(R.string.dbname_request)).child(getString(R.string.dbname_Chats)).child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                    request.setText("Requests(" + String.valueOf((int) dataSnapshot.getChildrenCount()) + ")");
+                else request.setText("Requests(" + String.valueOf(0) + ")");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        request.setOnClickListener(v -> {
+            Intent i = new Intent(mContext, Message_Request.class);
+            startActivity(i);
+        });
+        setupFirebaseAuth();
+        getUserList();
+    }
+
+    private void getUserList() {
+        userlist = new ArrayList<>();
+        myRef = FirebaseDatabase.getInstance().getReference(getString(R.string.dbname_Chats)).child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userlist.clear();
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    String user = snapshot1.getValue().toString();
+                    userlist.add(user);
+                }
+                loadChats(userlist);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    private void loadChats(List<String> userlist) {
+        chatList = new ArrayList<>();
+        for (int i = 0; i < userlist.size(); i++) {
+            DatabaseReference db = FirebaseDatabase.getInstance().getReference(getString(R.string.dbname_ChatList));
+            db.child(userlist.get(i)).orderByKey().limitToLast(1).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot dataSnapshot1 : snapshot.getChildren()) {
+                        Log.d(TAG, "onDataChange: chat" + dataSnapshot1.getValue());
+                        Chat chat = dataSnapshot1.getValue(Chat.class);
+                        chatList.add(chat);
+                    }
+                    if (chatList.size() == userlist.size()) {
+                        sortChatList(chatList, userlist);
+                        Log.d(TAG, "onDataChange: chat size1" + chatList.size());
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+    }
+
+    private void sortChatList(List<Chat> chatList, List<String> userlist) {
+        userlist2 = new ArrayList<>();
+        Collections.sort(chatList, (o1, o2) -> o2.getTim().compareTo(o1.getTim()));
+        for (int i = 0; i < chatList.size(); i++)
+            if (chatList.get(i).getSid().equals(FirebaseAuth.getInstance().getCurrentUser().getUid()))
+                this.userlist2.add(chatList.get(i).getRid());
+            else this.userlist2.add(chatList.get(i).getSid());
+        adaterChatList = new AdaterChatList(mContext, this.userlist2);
+        for (int i = 0; i < userlist2.size(); i++) lastMessage(userlist2.get(i));
+        displayChatList();
+    }
+
+
+    private void lastMessage(String uid) {
+        DatabaseReference refer1 = FirebaseDatabase.getInstance()
+                .getReference(getString(R.string.dbname_Chats));
+        refer1.child(FirebaseAuth.getInstance()
+                .getCurrentUser().getUid())
+                .child(uid)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        DatabaseReference refer = FirebaseDatabase.getInstance().getReference(getString(R.string.dbname_ChatList));
+                        refer.child(snapshot.getValue().toString())
+                                .orderByKey()
+                                .limitToLast(1)
+                                .addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        String lmsg = "default";
+                                        for (DataSnapshot ds : dataSnapshot.getChildren())
+                                            if (ds.exists()) {
+                                                Chat chat = ds.getValue(Chat.class);
+                                                lmsg = chat.getMsg();
+                                            }
+                                        adaterChatList.setLastMessage(uid, lmsg);
+                                        adaterChatList.notifyDataSetChanged();
+                                    }
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+    }
+
+    private void displayChatList() {
+        Log.d(TAG, "display first 10 chatslist");
+        paginateduserList = new ArrayList<>();
+        if (userlist2 != null) {
+            try {
+                int iteration = userlist2.size();
+                if (iteration > 10) iteration = 10;
+                mResults = 10;
+                for (int i = 0; i < iteration; i++) paginateduserList.add(userlist2.get(i));
+                Log.d(TAG, "chatslist: sss" + paginateduserList.size());
+                adaterChatList = new AdaterChatList(mContext, paginateduserList);
+                adaterChatList.setHasStableIds(true);
+                recyclerView.setAdapter(adaterChatList);
+                x++;
+            } catch (NullPointerException e) {
+                Log.e(TAG, "Null pointer exception" + e.getMessage());
+            } catch (IndexOutOfBoundsException e) {
+                Log.e(TAG, "index out of bound" + e.getMessage());
+            }
+        }
+    }
+
+    public void displayMoreChatList() {
+        Log.d(TAG, "display next 20 chatslist");
+        try {
+            if (userlist2.size() > mResults && userlist2.size() > 0) {
+                int iterations;
+                if (userlist2.size() > (mResults + 20)) {
+                    Log.d(TAG, "display next 20 chatslist");
+                    iterations = 20;
+                } else {
+                    Log.d(TAG, "display less tha 20 chatslist");
+                    iterations = userlist2.size() - mResults;
+                }
+                for (int i = mResults; i < mResults + iterations; i++)
+                    paginateduserList.add(userlist2.get(i));
+                recyclerView.post(() -> adaterChatList.notifyItemRangeInserted(mResults, iterations));
+                mResults = mResults + iterations;
+            }
+        } catch (NullPointerException e) {
+            Log.e(TAG, "Null pointer exception" + e.getMessage());
+
+        } catch (IndexOutOfBoundsException e) {
+            Log.e(TAG, "index out of bound" + e.getMessage());
+        }
+    }
+
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setup FirebaseAuth: setting up firebase auth.");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = firebaseAuth -> {
+            mUser = firebaseAuth.getCurrentUser();
+            if (mUser == null) {
+                Log.d(TAG, "onAuthStateChanged:signed_out");
+                Log.d(TAG, "onAuthStateChanged: navigating to login");
+                SharedPreferences settings = getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+                new android.app.AlertDialog.Builder(mContext)
+                        .setTitle("No user logon found")
+                        .setMessage("We will be logging u out. \n Please try to log in again")
+                        .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                            Intent intent = new Intent(mContext, login.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            settings.edit().clear().apply();
+                            if (mAuthListener != null) mAuth.removeAuthStateListener(mAuthListener);
+                            startActivity(intent);
+                        })
+                        .show();
+            } else Log.d(TAG, "onAuthStateChanged: signed_in:" + mUser.getUid());
+        };
+    }
+
+    private void setupBottomNavigationView() {
+        Log.d(TAG, " setupBottomNavigationView:setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.BottomNavViewBar);
+        BottomNaavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNaavigationViewHelper.enableNavigation(MessagesActivity.this, this, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) mAuth.removeAuthStateListener(mAuthListener);
+    }
+}
---
+++ app/src/main/java/com/orion/orion/NotificationActivity.java
@@ -0,0 +1,265 @@
+package com.orion.orion;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.orion.orion.Adapters.AdapterNotification2;
+import com.orion.orion.models.Notification;
+import com.orion.orion.util.BottomNaavigationViewHelper;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class NotificationActivity extends AppCompatActivity {
+    private static final String TAG = "mediafragment";
+    private static final int ACTIVITY_NUM = 3;
+
+    private Context mContext;
+
+    RecyclerView notificationRv;
+    private TextView clearNotification;
+    private int mResults;
+
+    private FirebaseAuth fAuth;
+    private ArrayList<Notification> notifyList;
+    private AdapterNotification2 adapterNotification2;
+    int x = 0;
+    private ArrayList<Notification> paginatedNotifications;
+    ImageView emptyNotification;
+    //    SP
+    Gson gson;
+    SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification2);
+        mContext = NotificationActivity.this;
+        notificationRv = findViewById(R.id.recycler_view);
+        clearNotification = findViewById(R.id.clearNotification);
+        emptyNotification = findViewById(R.id.emptyNotification);
+        setupBottomNavigationView();
+        //          Initialize SharedPreference variables
+        sp = mContext.getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+        gson = new Gson();
+        notificationRv.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext);
+        notificationRv.setLayoutManager(linearLayoutManager);
+        notifyList = new ArrayList<>();
+        notificationRv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (!recyclerView.canScrollVertically(1) && newState == RecyclerView.SCROLL_STATE_IDLE)
+                    displayMoreNotification();
+            }
+        });
+        fAuth = FirebaseAuth.getInstance();
+        getNotifcationFromSP();
+        clearNotification.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            builder.setMessage("You want to delete all the notification")
+                    .setTitle("Are you sure?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", (dialog, id) -> {
+                        notifyList.clear();
+                        if (paginatedNotifications != null) paginatedNotifications.clear();
+                        SharedPreferences.Editor editor = sp.edit().remove("nl");
+                        editor.apply();
+                        if (adapterNotification2 != null)
+                            adapterNotification2.notifyDataSetChanged();
+                        FirebaseUser user = fAuth.getCurrentUser();
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference(getString(R.string.dbname_users))
+                                .child(user.getUid())
+                                .child(getString(R.string.field_Notifications));
+                        reference.removeValue();
+                        //delete from database here then refresh for u naman
+                        displayMoreNotification();
+                    })
+                    .setNegativeButton("No", (dialog, id) -> dialog.cancel())
+                    .show();
+        });
+    }
+
+    //  fetching filtered notificationList  from SharedPreferences
+    private void getNotifcationFromSP() {
+        String json = sp.getString("nl", null);
+        Type type = new TypeToken<ArrayList<Notification>>() {
+        }.getType();
+        notifyList = gson.fromJson(json, type);
+        if (notifyList == null || notifyList.size() == 0) {    //        if no arrayList is present
+            notifyList = new ArrayList<>();
+            readNotification();  //            make new Arraylist
+        } else checkUpdate();
+
+    }
+
+    private void checkUpdate() {
+        DatabaseReference refer = FirebaseDatabase.getInstance().getReference(getString(R.string.dbname_users));
+        refer.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(getString(R.string.field_Notifications))
+                .orderByKey()
+                .limitToLast(1)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                            if (snapshot1.exists() && notifyList.size() != 0) {
+                                if (notifyList == null || notifyList.get(0).getTim().equals(snapshot1.getKey()))
+                                    displayNotification();
+                                else updateNotificationList();
+                            }
+                            displayNotification();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    private void updateNotificationList() {
+        Collections.reverse(notifyList);
+        DatabaseReference refer = FirebaseDatabase.getInstance().getReference(getString(R.string.dbname_users));
+        refer.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(getString(R.string.field_Notifications))
+                .orderByKey()
+                .startAt(notifyList.get(notifyList.size() - 1).getTim())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            int x = 0;
+                            for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                                x++;
+                                if (x == 1) continue;
+                                Notification notification = snapshot1.getValue(Notification.class);
+                                notifyList.add(notification);
+                            }
+                            Collections.reverse(notifyList);
+                            //                        Add newly Created ArrayList to Shared Preferences
+                            SharedPreferences.Editor editor = sp.edit();
+                            String json = gson.toJson(notifyList);
+                            editor.putString("nl", json);
+                            editor.apply();
+                            displayNotification();
+                        } else readNotification();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    private void readNotification() {
+        FirebaseUser user = fAuth.getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference(getString(R.string.dbname_users))
+                .child(user.getUid()).child(getString(R.string.field_Notifications));
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                notifyList.clear();
+                int x = 0;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    x++;
+                    Notification notification = snapshot.getValue(Notification.class);
+                    notifyList.add(notification);
+                    if (x == dataSnapshot.getChildrenCount()) {
+                        Collections.reverse(notifyList);
+//                        Add newly Created ArrayList to Shared Preferences
+                        SharedPreferences.Editor editor = sp.edit();
+                        String json = gson.toJson(notifyList);
+                        editor.putString("nl", json);
+                        editor.apply();
+                        displayNotification();
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void displayNotification() {
+        paginatedNotifications = new ArrayList<>();
+        if (notifyList != null) {
+
+            try {
+                int iteration = notifyList.size();
+                if (iteration == 0) emptyNotification.setVisibility(View.VISIBLE);
+                else {
+                    emptyNotification.setVisibility(View.GONE);
+                    if (iteration > 10) iteration = 10;
+                    mResults = 10;
+                    for (int i = 0; i < iteration; i++)
+                        paginatedNotifications.add(notifyList.get(i));
+                    adapterNotification2 = new AdapterNotification2(mContext, paginatedNotifications);
+                    adapterNotification2.setHasStableIds(true);
+                    notificationRv.setAdapter(adapterNotification2);
+                    x++;
+                }
+            } catch (NullPointerException e) {
+                Log.e(TAG, "Null pointer exception" + e.getMessage());
+            } catch (IndexOutOfBoundsException e) {
+                Log.e(TAG, "index out of bound" + e.getMessage());
+            }
+        }
+    }
+
+    public void displayMoreNotification() {
+        try {
+            if (notifyList.size() > mResults && notifyList.size() > 0) {
+                int iterations;
+                if (notifyList.size() > (mResults + 20)) iterations = 20;
+                else iterations = notifyList.size() - mResults;
+                for (int i = mResults; i < mResults + iterations; i++)
+                    paginatedNotifications.add(notifyList.get(i));
+                notificationRv.post(() -> adapterNotification2.notifyItemRangeInserted(mResults, iterations));
+                mResults = mResults + iterations;
+            }
+        } catch (NullPointerException e) {
+            Log.e(TAG, "Null pointer exception" + e.getMessage());
+        } catch (IndexOutOfBoundsException e) {
+            Log.e(TAG, "index out of bound" + e.getMessage());
+        }
+    }
+    private void setupBottomNavigationView() {
+        Log.d(TAG, " setupBottomNavigationView:setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.BottomNavViewBar);
+        BottomNaavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNaavigationViewHelper.enableNavigation(NotificationActivity.this, this, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+}
